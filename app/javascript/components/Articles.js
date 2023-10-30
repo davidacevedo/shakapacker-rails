bd1a38d30169f1e17d6e77c38de845798ed0263c
David <david.acevedo@appfolio.com>
2023-10-30 12:06:35 -0700

    UCSB live demo

--- app/javascript/components/Articles.js
+++ app/javascript/components/Articles.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { getArticles } from "../api/articles";
+import { useForm } from "react-hook-form";
 
 const Articles = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    reValidateMode: "onChange",
+  });
+  const onSubmit = async (data) => {
+    await createArticle(data.title, data.body);
+    refreshArticlesState();
+    reset();
+  };
+
   const [articles, setArticles] = React.useState([]);
 
   async function refreshArticlesState() {
@@ -15,7 +30,40 @@ const Articles = () => {
 
   return (
     <>
-      <p>{JSON.stringify(articles)}</p>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map((article) => {
+          return (
+            <li>
+              <h3>{article.title}</h3>
+              <p>{article.body}</p>
+            </li>
+          );
+        })}
+      </ul>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            placeholder="title"
+            {...register("title", { required: "Title is required" })}
+          />
+          <p style={{ color: "red" }}>{errors.title?.message}</p>
+        </div>
+        <div>
+          <label htmlFor="body">Body</label>
+          <input
+            type="text"
+            placeholder="body"
+            {...register("body", { required: "Body is required" })}
+          />
+          <p style={{ color: "red" }}>{errors.body?.message}</p>
+        </div>
+
+        <input type="submit" />
+      </form>
     </>
   );
 };
